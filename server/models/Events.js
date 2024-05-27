b7b346757c25c12d7c8c3ac37dba5af96005343c
Hadi <109927470+Hadi255255@users.noreply.github.com>
2024-05-27 21:07:44 +0300

    Update Events.js

--- server/models/Events.js
+++ server/models/Events.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+
+const eventSchema = mongoose.Schema({
+    email: {
+        type: String,
+    },
+    signupDate: {
+        type: Date,
+    },
+    signinDate: {
+        type: Date,
+    },
+    deleteAccountDate: {
+        type: Date,
+    },
+},);
+const Events = mongoose.model('Events', eventSchema);
+module.exports = Events;
