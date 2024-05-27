a4a5818e73b3fd78a7dccb72b9867761fd0aea4f
Hadi <109927470+Hadi255255@users.noreply.github.com>
2024-05-27 21:10:35 +0300

    Update checkAuth.js

--- server/middleware/checkAuth.js
+++ server/middleware/checkAuth.js
@@ -17,3 +17,16 @@ exports.isNotLoggedIn = function (req, res, next) {
         return res.send('<h1 style="text-align:center">Access Denied</h1>')
     }
 }
+exports.isDirector = function (req, res, next) {
+    // console.log('req.user ', req.user);
+    if (req.user) {
+        if (req.user.email == "engineer.shadirahhal@gmail.com") {
+            next();
+        }
+        else {
+            return res.send('<h1 style="text-align:center">Access Denied</h1>')
+        }
+    } else {
+        return res.send('<h1 style="text-align:center">Access Denied</h1>')
+    }
+}
